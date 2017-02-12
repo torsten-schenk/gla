a3179881cb77f7e70553ca3b5598642ea2d444a1
Torsten Schenk <schule@schenk.biz>
2017-02-12 15:08:17 +0100

    squirrel std math lib added

--- src/sqstdlib/module.c
+++ src/sqstdlib/module.c
@@ -2,6 +2,7 @@
 #include <apr_pools.h>
 #include <gla/squirrel.h>
 #include <sqstdstring.h>
+#include <sqstdmath.h>
 
 #include "module.h"
 
@@ -15,6 +16,20 @@ static int push_stringlib(
 
 	sq_newtable(vm);
 	sqstd_register_stringlib(vm);
+
+	return GLA_SUCCESS;
+}
+
+static int push_mathlib(
+		gla_rt_t *rt,
+		void *module_data_,
+		apr_pool_t *pool,
+		apr_pool_t *tmp)
+{
+	HSQUIRRELVM vm = gla_rt_vm(rt);
+
+	sq_newtable(vm);
+	sqstd_register_mathlib(vm);
 
 	return GLA_SUCCESS;
 }
@@ -33,6 +48,14 @@ int gla_mod_sqstdlib_register(
 	if(ret != GLA_SUCCESS)
 		return ret;
 	ret = gla_rt_mount_object(rt, &path, push_stringlib, NULL);
+	if(ret != GLA_SUCCESS)
+		return ret;
+
+	path = *root;
+	ret = gla_path_parse_append_entity(&path, "<" GLA_ENTITY_EXT_OBJECT ">mathlib", pool);
+	if(ret != GLA_SUCCESS)
+		return ret;
+	ret = gla_rt_mount_object(rt, &path, push_mathlib, NULL);
 	if(ret != GLA_SUCCESS)
 		return ret;
 
